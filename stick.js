908f5083f7a09a3718f4240a99cf954ec2a27802
jeannie,lin <jeannie.lin@hotmail.com>
2014-04-24 12:23:20 +0800

    Implement sticker with textarea, which would useful to update data to server
    Signed-off-by: jeannie,lin <jeannie.lin@hotmail.com>

--- stick.js
+++ stick.js
@@ -2,8 +2,9 @@ $(document).ready(function(){
 	var fall = function(opts){
 		var options = $.extend({
 				width : 220,
-				vOffset : 15,
-				hOffset : 20,
+				margin : 190,
+				offsetY : 15,
+				offsetX : 20,
 				delay : 0,
 				align : 'center',
 				container : '#main',
@@ -13,7 +14,7 @@ $(document).ready(function(){
 				nrow : 0,
 				ncol : 0,
 				conHeight : 0,
-				index : 0,
+				count : 0,
 				timer : []
 			},opts)
 
@@ -38,14 +39,14 @@ $(document).ready(function(){
 
 		function top(index) {
 			var index = index || 0;
-			var top = options.vOffset;
+			var top = options.offsetY;
 			var owner = $(options.owner);
 			var nrow = Math.floor(index / options.ncol);
 
 			index %= options.ncol;
 
 			for(var r=0;r<nrow;r++) {
-				top += parseInt(owner.eq(r*options.ncol+index).height(),10) +30 + options.vOffset;
+				top += owner.get(r*options.ncol+index).clientHeight + options.offsetY;
 			}
 
 			owner = null;
@@ -54,20 +55,20 @@ $(document).ready(function(){
 
 		function left(index) {
 			var nrow = Math.floor(index / options.ncol);
-			return (index - options.ncol * nrow )*(options.width+options.hOffset) + options.hOffset;
+			return (index - options.ncol * nrow )*(options.width+options.offsetX) + options.offsetX;
 		}
 
 		function conHeight() {
 			var start = options.nrow * options.ncol;
-			var count = options.index % options.ncol + 1;
-			var heights = [parseInt($('body').height())-190];
+			var count = options.count % options.ncol + 1;
+			var heights = [parseInt($('body').height())-options.margin];
 			var owner = $(options.owner);
 			var container = $(options.container);
 			var current;
 
 			for(var i=0;i<count;i++) {
 				current = owner.get(start+i);
-				current && heights.push(parseInt(current.style.top)+current.clientHeight + options.vOffset)
+				current && heights.push(parseInt(current.style.top)+current.clientHeight + options.offsetY)
 			}
 
 			owner = container = current = null;
@@ -75,11 +76,11 @@ $(document).ready(function(){
 		}
 
 		function conWidth() {
-			return options.ncol  * (options.width+options.hOffset) + options.hOffset;
+			return options.ncol  * (options.width+options.offsetX) + options.offsetX;
 		}
 
 		function init() {
-			options.ncol = Math.floor($('body').width() / (options.width + options.hOffset));
+			options.ncol = Math.floor($('body').width() / (options.width + options.offsetX));
 			options.nrow = 0;
 			repaint(0);
 		}
@@ -92,24 +93,50 @@ $(document).ready(function(){
 		}
 
 		function add(idx) {
-			$(options.adder).before('<div class="stick"><p class="edit" contenteditable="true"></p><p class="time"></p><div class="close">X</div></div>');
-			$(options.owner).eq(idx).css({'background-color':color()}).find('.time').html('--'+date()).end().find(options.deler).click(
-				function(){
-					var idx = $(this.parentNode).index();
-					$(options.owner).eq(idx).remove();
-					options.index -= 1;
-					$(options.container).hasClass('scroll') ? reset():!$(options.container).hasClass('random') && layout(function(index){return index > idx-1});
+			$(options.adder).before('<div class="stick"><p class="display"></p> \
+				<p class="time">--'+date()+'</p> \
+				<p class="close">X</p></div>');
 
-				}).end().find('.edit').bind('input',function(){
-					var idx = $(this.parentNode).index();
-					layout(function(index){
-						return index > idx && (idx % options.ncol === index % options.ncol)
+			$(options.owner).eq(idx).css(
+					{'background-color':color()}
+				).find(options.deler).click(
+					function(){
+						var idx = $(this.parentNode).index();
+						$(options.owner).eq(idx).remove();
+						options.count -= 1;
+						$(options.container).hasClass('scroll') ? reset():!$(options.container).hasClass('random') && layout(function(index){return index > idx-1});
+
+					}).end().find('.display').bind('click',function(){
+						var val = $(this).html().replace(/&lt;/g,'<').replace(/&gt;/g,'>');
+						$(this).after('<textarea id="edit" class="edit"></textarea>').siblings('#edit').bind({
+							'focus':function(){
+								$(this).trigger('height');
+							},
+							'height':function(){
+								console.info($(this).css('rows'))
+								$(this).css('height',this.scrollHeight +'px');
+							},
+							'input':function(){
+								$(this).trigger('height');
+							},
+							'change':function(){
+								
+							},
+							'blur':function(){
+								var val = $(this).val().replace(/</g,'&lt;').replace(/>/g,'&gt;');
+								var idx = $(this.parentNode).index();
+
+								$(this).siblings('.display').html(val).show().end().remove();
+								layout(function(index){
+									return index > idx && (idx % options.ncol === index % options.ncol);
+								})
+
+							}
+						}).val(val).focus().end().hide();
 					})
-					
-				});
 
 			options.nrow = Math.floor(idx/options.ncol);
-			options.index += 1;
+			options.count += 1;
 			repaint(idx);
 			options.nrow = Math.floor((idx+1)/options.ncol);
 			repaint(idx+1);
@@ -123,7 +150,7 @@ $(document).ready(function(){
 		function layout(filter) {
 			var filter = filter || function(){return true;}
 
-			options.ncol = Math.floor($('body').width() / (options.width + options.hOffset));
+			options.ncol = Math.floor($('body').width() / (options.width + options.offsetX));
 			options.nrow = Math.floor($('#new').index()/options.ncol);
 
 			$(options.owner).filter(filter).each(function(){
@@ -143,7 +170,7 @@ $(document).ready(function(){
 			$(options.container).addClass('random')
 			$(options.owner).each(function(){
 				var p = $(this.parentNode);
-				$(this).css({left:random(p.width()-$(this).width()-options.hOffset,1),top:random(p.height()-$(this).height()-options.vOffset,1)});
+				$(this).css({left:random(p.width()-$(this).width()-options.offsetX,1),top:random(p.height()-$(this).height()-options.offsetY,1)});
 			})
 		}
 
@@ -195,7 +222,7 @@ $(document).ready(function(){
 
 			for(var i=0;i<=options.nrow;i++) {
 				idx = i*options.ncol+col;
-				idx <= options.index && heights.push(owner.eq(idx).height() + 30 + options.vOffset);
+				idx <= options.count && heights.push(owner.eq(idx).height() + 30 + options.offsetY);
 			}
 
 			return heights;
@@ -212,12 +239,12 @@ $(document).ready(function(){
 				var top = parseInt($(options.owner).get(col).style.top);
 				var val = 0,l=hs.length-1;
 
-				if(top <= options.vOffset) {
+				if(top <= options.offsetY) {
 					return 0
 				}
 
 				for(var i=l;i>=0;i--) {
-					val += hs[i] + options.vOffset;
+					val += hs[i] + options.offsetY;
 					if (val >= top) {
 						return i
 					}
@@ -230,12 +257,12 @@ $(document).ready(function(){
 				var top = parseInt($(options.owner).get(col).style.top);
 				var val = 0,l=hs.length-1;
 
-				if(top <= options.vOffset) {
+				if(top <= options.offsetY) {
 					return l
 				}
 
 				for(var i=l;i>=0;i--) {
-					val += hs[i] + options.vOffset;
+					val += hs[i] + options.offsetY;
 					if (val >= top) {
 						return (i < 1) ? l : i-1;
 					}
@@ -246,15 +273,15 @@ $(document).ready(function(){
 
 			function top(top) {
 				if (up) {
-					return (top > options.vOffset) ? top - hs[f] : h - hs[i] + options.vOffset
+					return (top > options.offsetY) ? top - hs[f] : h - hs[i] + options.offsetY
 				} else {
-					return (top + hs[f] < h) ? top + hs[f] : options.vOffset
+					return (top + hs[f] < h) ? top + hs[f] : options.offsetY
 				}
 			}
 
 			for(i=0;i<=options.nrow;i++) {
 				idx = col+i*options.ncol;
-				if (idx > options.index)
+				if (idx > options.count)
 					break
 
 				owner.eq(idx).css({'top':top(parseInt(owner.get(idx).style.top))});
